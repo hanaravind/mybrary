38e97033562a0d0d8605e145d79dc3a44b3e057a
Aravind SJ <hanaravind5@gmail.com>
2022-01-11 00:27:32 +0530

    initial commit added

---
+++ app.js
@@ -0,0 +1,34 @@
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').parse()
+}
+
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const indexRoutes = require('./routes/index')
+const port = process.env.PORT || 3000
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(express.urlencoded({extended:false}));
+
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+app.use(indexRoutes)
+
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true
+})
+const db = mongoose.connection
+
+db.on('error', error => console.log(error))
+db.once('open', () => console.log('connected'))
+
+
+app.listen(3000, () => {
+    console.log('server listening on ', port)
+})
